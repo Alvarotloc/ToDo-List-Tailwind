5d9a86c1f4d1b3682ce50dd7ab36c68e2be97c80
Alvarotloc <alvarotloc@gmail.com>
2022-06-03 19:47:51 +0200

    Header creado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Header from './components/Header';
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Header />
+    </main>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-purple-400 py-10">
+        <h1 className="text-4xl text-white font-bold text-center">ToDo List</h1>
+    </header>
   )
 }
 
